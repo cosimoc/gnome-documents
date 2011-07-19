db29f52c3493293ae6a5ea0738b6d25aba51d1ba
Cosimo Cecchi <cosimoc@gnome.org>
2011-07-19 14:48:22 -0400

    application: don't use the dark theme

--- src/application.js
+++ src/application.js
@@ -97,9 +97,6 @@ Application.prototype = {
     },
 
     _initGtkSettings: function() {
-        let settings = Gtk.Settings.get_default();
-        settings.gtk_application_prefer_dark_theme = true;
-
         let provider = new Gtk.CssProvider();
         provider.load_from_path(Path.STYLE_DIR + "gtk-style.css");
         Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
