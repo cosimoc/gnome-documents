fd255438c2039a8b0d5fe8926a62d81c84df2ca9
Florian Müllner <fmuellner@gnome.org>
2011-12-14 21:40:40 +0100

    application: Make sure set_prgname() is called in every instance
    Calling GLib.set_prgname() in startup() means that the program
    name is only set correctly in the primary instance - move the
    call, so that all instances end up with the correct program
    name.

--- src/application.js
+++ src/application.js
@@ -62,6 +62,7 @@ Application.prototype = {
             application_id: 'org.gnome.Documents',
             flags: Gio.ApplicationFlags.HANDLES_COMMAND_LINE
         });
+        GLib.set_prgname('gnome-documents');
         this.application.connect('startup', Lang.bind(this, this._onStartup));
         this.application.connect('command-line', Lang.bind(this, this._commandLine));
         this.application.connect('activate', Lang.bind(this,
@@ -75,7 +76,6 @@ Application.prototype = {
         Gettext.textdomain('gnome-documents');
         String.prototype.format = Format.format;
 
-        GLib.set_prgname('gnome-documents');
         GtkClutter.init(null, null);
         EvDoc.init();
         Tweener.init();
