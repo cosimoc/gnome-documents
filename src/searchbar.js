11901251196d80a7e382d89af86e52ef100e06e5
Cosimo Cecchi <cosimoc@gnome.org>
2012-06-11 00:27:38 -0400

    query: fix typo

--- src/searchbar.js
+++ src/searchbar.js
@@ -141,7 +141,7 @@ const SearchTypeManager = new Lang.Class({
                                       where: '?urn rdf:type nfo:DataContainer .' }));
         this.addItem(new SearchType({ id: 'pdf',
                                       name: _("PDF Documents"),
-                                      filter: 'fn:contains(nie:mimeType(?urn), \"application/pdf\"',
+                                      filter: 'fn:contains(nie:mimeType(?urn), \"application/pdf\")',
                                       where: '?urn rdf:type nfo:PaginatedTextDocument .' }));
         this.addItem(new SearchType({ id: 'presentations',
                                       name: _("Presentations"),
